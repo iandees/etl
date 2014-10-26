50ff9a9cc00cb23c5938d63c03fbbb453b5ba588
Ian Dees <ian.dees@gmail.com>
2014-10-26 19:14:20 -0400

    Use OGR, check for existing files, start with conform task

--- etl/main.py
+++ etl/main.py
@@ -3,11 +3,14 @@ import errno
 import json
 import urllib
 import urllib2
-import datetime
-import tempfile
+import socket
+import csv
 
 from urlparse import urlparse
 from zipfile import ZipFile
+
+from osgeo import ogr, osr
+ogr.UseExceptions()
 
 import logging
 logging.basicConfig(level=logging.DEBUG)
@@ -53,6 +56,13 @@ class Urllib2ExtractionTask(ExtractionTask):
         mkdirsp(download_path)
 
         for source_url in source_urls:
+            file_path = os.path.join(download_path, source_url.split('/')[-1])
+
+            if os.path.exists(file_path):
+                output_files.append(file_path)
+                self.logger.debug("File exists %s", file_path)
+                continue
+
             self.logger.debug("Requesting %s", source_url)
             headers = {'User-Agent': self.USER_AGENT}
 
@@ -62,7 +72,6 @@ class Urllib2ExtractionTask(ExtractionTask):
             except urllib2.URLError as e:
                 raise ExtractionError("Could not connect to URL", e)
 
-            file_path = os.path.join(download_path, resp.geturl().split('/')[-1])
 
             size = 0
             with open(file_path, 'wb') as fp:
@@ -73,7 +82,7 @@ class Urllib2ExtractionTask(ExtractionTask):
                         break
                     fp.write(chunk)
 
-                output_files.append(file_path)
+            output_files.append(file_path)
 
             self.logger.info("Downloaded %s bytes for file %s", size, file_path)
 
@@ -118,8 +127,14 @@ class EsriRestExtractionTask(ExtractionTask):
         for source_url in source_urls:
             size = 0
             parts = urlparse(source_url)
-            source_downloaded = os.path.join(download_path, parts.netloc + '.json')
-            with open(source_downloaded, 'w') as f:
+            file_path = os.path.join(download_path, parts.netloc + '.json')
+
+            if os.path.exists(file_path):
+                output_files.append(file_path)
+                self.logger.debug("File exists %s", file_path)
+                continue
+
+            with open(file_path, 'w') as f:
                 f.write('{\n"type": "FeatureCollection",\n"features": [\n')
                 start = 0
                 width = 500
@@ -143,6 +158,8 @@ class EsriRestExtractionTask(ExtractionTask):
                         resp = urllib2.urlopen(req, timeout=10)
                     except urllib2.URLError as e:
                         raise ExtractionError("Could not connect to URL", e)
+                    except socket.timeout as e:
+                        raise ExtractionError("Timeout when connecting to URL", e)
 
                     data = json.load(resp)
                     geometry_type = data.get('geometryType')
@@ -161,8 +178,8 @@ class EsriRestExtractionTask(ExtractionTask):
                         start += width
 
                 f.write('\n]\n}\n')
-            self.logger.info("Downloaded %s ESRI features for file %s", size, source_downloaded)
-            output_files.append(source_downloaded)
+            self.logger.info("Downloaded %s ESRI features for file %s", size, file_path)
+            output_files.append(file_path)
         return output_files
 
 
@@ -207,8 +224,81 @@ class ZipDecompressTask(DecompressionTask):
         return output_files
 
 
+class ConvertToCsvTask(object):
+
+    logger = logger.getChild('convert')
+
+    known_types = ('.shp', '.json', '.csv', '.kml')
+
+    def convert(self, source_key, source_paths):
+        output_files = []
+        convert_path = os.path.join('.', 'workdir', source_key, 'converted')
+        mkdirsp(convert_path)
+
+        for source_path in source_paths:
+            filename = os.path.basename(source_path)
+            basename, ext = os.path.splitext(filename)
+            file_path = os.path.join(convert_path, basename + '.csv')
+
+
+            if ext not in self.known_types:
+                self.logger.debug("Skipping %s because I don't know how to convert it", source_path)
+                continue
+            if os.path.exists(file_path):
+                output_files.append(file_path)
+                self.logger.debug("File exists %s", file_path)
+                continue
+
+            in_datasource = ogr.Open(source_path, 0)
+            in_layer = in_datasource.GetLayer()
+            inSpatialRef = in_layer.GetSpatialRef()
+
+            in_layer_defn = in_layer.GetLayerDefn()
+            out_fieldnames = []
+            for i in range(0, in_layer_defn.GetFieldCount()):
+                field_defn = in_layer_defn.GetFieldDefn(i)
+                out_fieldnames.append(field_defn.GetName())
+            out_fieldnames.append('centroid')
+
+            outSpatialRef = osr.SpatialReference()
+            outSpatialRef.ImportFromEPSG(4326)
+            coordTransform = osr.CoordinateTransformation(inSpatialRef, outSpatialRef)
+
+            with open(file_path, 'w') as f:
+                writer = csv.DictWriter(f, fieldnames=out_fieldnames)
+                writer.writeheader()
+
+                in_feature = in_layer.GetNextFeature()
+                while in_feature:
+                    row = dict()
+
+                    for i in range(0, in_layer_defn.GetFieldCount()):
+                        field_defn = in_layer_defn.GetFieldDefn(i)
+                        row[field_defn.GetNameRef()] = in_feature.GetField(i)
+                    geom = in_feature.GetGeometryRef()
+                    geom.Transform(coordTransform)
+                    row['centroid'] = geom.Centroid().ExportToWkt()
+
+                    writer.writerow(row)
+
+                    in_feature.Destroy()
+                    in_feature = in_layer.GetNextFeature()
+
+            in_datasource.Destroy()
+            output_files.append(file_path)
+
+        return output_files
+
+
+class ConformTask(object):
+
+    logger = logger.getChild('conform')
+
+    def conform(self, source_key, source_paths):
+
+
 def skip(k):
-    if k.startswith('au'):
+    if not k.startswith('us'):
         return True
 
 for dirpath, dirnames, filenames in os.walk('/Users/iandees/Workspace/addresses/addresses/sources'):
@@ -220,6 +310,7 @@ for dirpath, dirnames, filenames in os.walk('/Users/iandees/Workspace/addresses/
 
                 if skip(source_key): continue
 
+                # Download the source data
                 try:
                     task = ExtractionTask.from_type_string(source_data.get('type'))
 
@@ -235,6 +326,7 @@ for dirpath, dirnames, filenames in os.walk('/Users/iandees/Workspace/addresses/
                     print "Problem extracting {}: {}".format(f, e)
                     continue
 
+                # Decompress the downloaded file
                 try:
                     task = DecompressionTask.from_type_string(source_data.get('compression'))
 
@@ -242,3 +334,13 @@ for dirpath, dirnames, filenames in os.walk('/Users/iandees/Workspace/addresses/
                 except KeyError:
                     print "Don't know how to decompress {}".format(f)
                     continue
+
+                # Convert the source data to a CSV
+                task = ConvertToCsvTask()
+                filenames = task.convert(source_key, filenames)
+
+                # Now we map the fields in the CSV to OpenAddresses field names
+                task = ConformTask()
+                filenames = task.conform(source_key, filenames)
+
+                print filenames
